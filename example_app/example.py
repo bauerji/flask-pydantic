b37978341283f3d4cbda3857c70a6625ad70e22a
Jiri Bauer <baueji@gmail.com>
2021-01-31 10:22:46 +0100

    minor refactor

--- example_app/example.py
+++ example_app/example.py
@@ -1,27 +1,28 @@
 from typing import Optional
-from flask import Flask, request
+from flask import Flask
 from pydantic import BaseModel
 
 from flask_pydantic import validate
 
 app = Flask("flask_pydantic_app")
 
+
 class RequestBodyModel(BaseModel):
-	name: str
-	nickname: Optional[str]
+    name: str
+    nickname: Optional[str]
 
 
 class QueryModel(BaseModel):
-	age: int
+    age: int
+
 
 @app.route("/", methods=["GET"])
 @validate()
-def get(query:QueryModel):
-	age = query.age
-	return ResponseModel(
-		age=age,
-		id=0, name="abc", nickname="123"
-		)
+def get(query: QueryModel):
+    age = query.age
+    return ResponseModel(age=age, id=0, name="abc", nickname="123")
+
+
 """
 curl --location --request GET 'http://127.0.0.1:5000/'
 curl --location --request GET 'http://127.0.0.1:5000/?ageeee=5'
@@ -31,21 +32,20 @@ curl --location --request GET 'http://127.0.0.1:5000/?age=5'
 """
 
 
-
 class ResponseModel(BaseModel):
-	id: int
-	age: int
-	name: str
-	nickname: Optional[str]
+    id: int
+    age: int
+    name: str
+    nickname: Optional[str]
+
 
 @app.route("/", methods=["POST"])
 @validate()
-def post(body:RequestBodyModel): 
-	name = body.name
-	nickname = body.nickname
-	return ResponseModel(
-		name=name, nickname=nickname,id=0, age=1000
-		)
+def post(body: RequestBodyModel):
+    name = body.name
+    nickname = body.nickname
+    return ResponseModel(name=name, nickname=nickname, id=0, age=1000)
+
 
 """
 curl --location --request POST 'http://127.0.0.1:5000/'
@@ -63,16 +63,15 @@ curl --location --request POST 'http://127.0.0.1:5000/' \
 --data-raw '{"name":123}'
 """
 
+
 @app.route("/both", methods=["POST"])
 @validate()
-def get_and_post(body:RequestBodyModel,query:QueryModel):
-	name = body.name # From request body
-	nickname = body.nickname # From request body
-	age = query.age # from query parameters
-	return ResponseModel(
-		age=age, name=name, nickname=nickname,
-		id=0
-	)
+def get_and_post(body: RequestBodyModel, query: QueryModel):
+    name = body.name  # From request body
+    nickname = body.nickname  # From request body
+    age = query.age  # from query parameters
+    return ResponseModel(age=age, name=name, nickname=nickname, id=0)
+
 
 """
 curl --location --request POST 'http://127.0.0.1:5000/both' \
